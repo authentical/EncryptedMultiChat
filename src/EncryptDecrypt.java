399f5cf653ec84160714b7efd29f7f3cd5e54e00
Chris Spencer <cvspencer@gmail.com>
2018-02-15 02:39:52 +0900

    Added EncryptDecrypt class

---
+++ src/EncryptDecrypt.java
@@ -0,0 +1,42 @@
+// From http://www.adeveloperdiary.com/java/how-to-easily-encrypt-and-decrypt-text-in-java/
+// I do not believe this is a compliant (really secure) way to handle the encryption but its
+// my first go.
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class EncryptDecrypt {
+    public static String encrypt(String strClearText,String strKey) throws Exception{
+        String strData="";
+
+        try {
+            SecretKeySpec skeyspec=new SecretKeySpec(strKey.getBytes(),"Blowfish");
+            Cipher cipher=Cipher.getInstance("Blowfish");
+            cipher.init(Cipher.ENCRYPT_MODE, skeyspec);
+            byte[] encrypted=cipher.doFinal(strClearText.getBytes());
+            strData=new String(encrypted);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception(e);
+        }
+        return strData;
+    }
+
+    public static String decrypt(String strEncrypted,String strKey) throws Exception{
+        String strData="";
+
+        try {
+            SecretKeySpec skeyspec=new SecretKeySpec(strKey.getBytes(),"Blowfish");
+            Cipher cipher= Cipher.getInstance("Blowfish");
+            cipher.init(Cipher.DECRYPT_MODE, skeyspec);
+            byte[] decrypted=cipher.doFinal(strEncrypted.getBytes());
+            strData=new String(decrypted);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception(e);
+        }
+        return strData;
+    }
+}
